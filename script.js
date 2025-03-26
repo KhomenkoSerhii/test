43fb1962ebc965e7ea877b5175b291beffdc5a17
KhomenkoSerhii <cahxoce2101@gmail.com>
2025-03-26 14:41:27 +0200

    text v3

--- script.js
+++ script.js
@@ -72,81 +72,77 @@ const createCardElement = ({ companies, experience, location }, index) => {
   card.classList.add("tinder--card");
   card.style.zIndex = 100 - index;
 
-  // Generate company details dynamically
-  const companyDetails = companies
-    .map(
-      (company) => `<div class="company">
-      <div class="company--header">
-      <img src="${getCompanyFavicon(company.url)}" alt="Company Logo" />
-          <h3>${company.name}</h3>
-          </div>
-          <p>${company.position}</p>
-          </div>
-        `
-    )
-    .join("");
-
   // Card content
   card.innerHTML = `
   <article>
-  <div class="details">
-      ${companyDetails}
-      </div>
-     
-      <div class="additional">
-        <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="18" viewBox="0 0 19 18" fill="none">
-  <path d="M15.8325 7.5C15.8325 11.2448 11.6783 15.1447 10.2833 16.3492C10.1533 16.447 9.99512 16.4998 9.83252 16.4998C9.66992 16.4998 9.51173 16.447 9.38177 16.3492C7.98677 15.1447 3.83252 11.2448 3.83252 7.5C3.83252 5.9087 4.46466 4.38258 5.58988 3.25736C6.7151 2.13214 8.24122 1.5 9.83252 1.5C11.4238 1.5 12.9499 2.13214 14.0752 3.25736C15.2004 4.38258 15.8325 5.9087 15.8325 7.5Z" stroke="#A5A7A5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M9.83252 9.75C11.0752 9.75 12.0825 8.74264 12.0825 7.5C12.0825 6.25736 11.0752 5.25 9.83252 5.25C8.58988 5.25 7.58252 6.25736 7.58252 7.5C7.58252 8.74264 8.58988 9.75 9.83252 9.75Z" stroke="#A5A7A5" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-        <p>Location:</p>
+    <div class="details">
+      <div class="company">
+        <div class="company--header">
+          <img src="${getCompanyFavicon(
+            companies[0].url
+          )}" alt="Company Logo" />
+          <h3>${companies[0].name}</h3>
         </div>
-        <span>${location}</span>
+        <h1>${companies[0].position}</h1>
       </div>
+    </div>
+
+    <div class="card-info">
+    <div class="additional">
+      <div>
+        <p>Previous role:</p>
+        <div>
+          <span>${companies[0].position}, &nbsp</span>
+          <span>${companies[0].name}</span>
+          <img src="${getCompanyFavicon(
+            companies[0].url
+          )}" alt="Company Logo" />
+        </div>
+      </div>
+    </div>
 
     <div class="additional">
-        <div>
-<svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
-  <path d="M12.8325 6V18" stroke="#A5A7A5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M18.0287 9L7.63672 15" stroke="#A5A7A5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M7.63672 9L18.0287 15" stroke="#A5A7A5" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-        <p>Experience:</p>
-        </div>
-        <span>${experience} ${
+      <p>Location:</p>
+      <span>${location}</span>
+    </div>
+
+    <div class="additional">
+      <p>Experience:</p>
+      <span>${experience} ${
     experience > 1 ? "yrs of experience" : "year of experience"
   }</span>
-      </div>
-</article>
+    </div>
+    </div>
+  </article>
 
-      <div class="tinder--buttons">
+  <div class="tinder--buttons">
     <button class="decline">
-          <svg width="71" height="70" viewBox="0 0 71 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="35.8325" cy="35" r="34.5" fill="url(#paint0_linear_7034_2746)" stroke="#71000D"/>
-            <path d="M46.3325 24.75L25.3325 45.75" stroke="#71000D" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M25.3325 24.75L46.3325 45.75" stroke="#71000D" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <defs>
-              <linearGradient id="paint0_linear_7034_2746" x1="70.8325" y1="70" x2="32.0857" y2="4.27577" gradientUnits="userSpaceOnUse">
-                <stop stop-color="white"/>
-                <stop offset="1" stop-color="white" stop-opacity="0.6"/>
-              </linearGradient>
-            </defs>
-          </svg>
-        </button>
-        <button class="approve">
-          <svg width="71" height="70" viewBox="0 0 71 70" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="35.8325" cy="35" r="35" fill="url(#paint0_linear_7034_2742)"/>
-            <path d="M49.8325 24.75L30.5825 44L21.8325 35.25" stroke="white" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round"/>
-            <defs>
-              <linearGradient id="paint0_linear_7034_2742" x1="70.8325" y1="70" x2="41.0133" y2="-15.9787" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#00B140"/>
-                <stop offset="1" stop-color="#84F998"/>
-              </linearGradient>
-            </defs>
-          </svg>
-        </button>
-      </div>
-    `;
+      <svg width="71" height="70" viewBox="0 0 71 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="35.8325" cy="35" r="34.5" fill="url(#paint0_linear_7034_2746)" stroke="#71000D" />
+        <path d="M46.3325 24.75L25.3325 45.75" stroke="#71000D" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M25.3325 24.75L46.3325 45.75" stroke="#71000D" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round" />
+        <defs>
+          <linearGradient id="paint0_linear_7034_2746" x1="70.8325" y1="70" x2="32.0857" y2="4.27577" gradientUnits="userSpaceOnUse">
+            <stop stop-color="white" />
+            <stop offset="1" stop-color="white" stop-opacity="0.6" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </button>
+    <button class="approve">
+      <svg width="71" height="70" viewBox="0 0 71 70" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="35.8325" cy="35" r="35" fill="url(#paint0_linear_7034_2742)" />
+        <path d="M49.8325 24.75L30.5825 44L21.8325 35.25" stroke="white" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round" />
+        <defs>
+          <linearGradient id="paint0_linear_7034_2742" x1="70.8325" y1="70" x2="41.0133" y2="-15.9787" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#00B140" />
+            <stop offset="1" stop-color="#84F998" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </button>
+  </div>
+`;
   return card;
 };
 
